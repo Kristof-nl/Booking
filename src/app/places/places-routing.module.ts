59b463d604dc3d29867def88aa6bb5b6e1b5a88a
Kristof-nl <hermaszewski.krzysztof@gmail.com>
2023-01-07 19:01:27 +0100

    Start

---
+++ src/app/places/places-routing.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PlacesPage } from './places.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PlacesPage
+  },
+  {
+    path: 'discover',
+    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
+  },
+  {
+    path: 'offers',
+    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
+  },
+  {
+    path: 'tabs',
+    component: PlacesPage,
+    children: [
+      { path: 'discover', children: [
+        {
+          path: '',
+          loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
+        },
+        {
+          path: ':placeId',
+          loadChildren: () => import('./discover/place-detail/place-detail.module').then( m => m.PlaceDetailPageModule)
+        }
+      ]},
+      { path: 'offers', children: [
+        {
+          path: '',
+          loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
+        },
+        {
+          path: 'new',
+          loadChildren: () => import('./offers/new-offer/new-offer.module').then( m => m.NewOfferPageModule)
+        },
+        {
+          path: '',
+          loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
+        }
+      ]}
+    ]
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PlacesPageRoutingModule {}
